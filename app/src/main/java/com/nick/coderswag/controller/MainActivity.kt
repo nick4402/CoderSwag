21c00c4019b343d22843b265e3a37f23ad4eb0e8
kittichai <nick4402@gmail.com>
2021-02-01 11:09:05 +0700

    refactored for recyele view

--- app/src/main/java/com/nick/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/nick/coderswag/controller/MainActivity.kt
@@ -4,21 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.nick.coderswag.R
 import com.nick.coderswag.adapter.CategoryAdapter
+import com.nick.coderswag.adapter.CategoryRecycleAdapter
 import com.nick.coderswag.model.Category
 import com.nick.coderswag.service.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this,DataService.categories)
+        adapter = CategoryRecycleAdapter(this,DataService.categories)
         categoryList.adapter = adapter
 
         /*
@@ -28,5 +30,9 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this,"You clicked on the ${category.title} cell",Toast.LENGTH_SHORT).show()
         }
          */
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryList.layoutManager = layoutManager
+        categoryList.setHasFixedSize(true)
     }
 }
